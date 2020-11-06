933474b7741bcb2e116acaccf0155601f9ae091e
ben noiof <ben12.nk@gmail.com>
2020-11-06 13:44:02 +0200

    test

---
+++ Obj.java
@@ -0,0 +1,16 @@
+
+public class Obj {
+private int x;
+private char c;
+private boolean b;
+
+public Obj(int _x) {
+	x = _x;
+}
+public Obj(int _x, int y) {
+	x = 20;
+}
+public int getX() {
+	return x;
+}
+}
---
+++ STUDENT.java
@@ -0,0 +1,39 @@
+
+public class HW_1 {
+	
+
+	private int idd;
+	private String name;
+	
+	public HW_1() {
+		idd = 0;
+		name = "Temp";
+	}
+	public HW_1(int id, String _name) {
+		this.idd = id;
+		name = _name;	
+	}
+
+	public HW_1 (int _id) {
+		this.idd = _id;
+		name = "temp";
+	}
+	
+	public void ChangeName(String s) {
+		name = s;
+			
+	}
+	
+	public String toString() {
+		return idd+" :id | name : "+name;
+	}
+
+
+	public static void main(String[] args) {
+		HW_1 s = new HW_1(123213,"ben");
+		System.out.print(s+"\n");
+		s.ChangeName("alex");
+		System.out.print(s);
+	}
+
+}
